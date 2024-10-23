7015298e4b9ea408567dc958c557fc017c72eadd
vishutanku <rokomunna28@gmail.com>
2024-10-24 00:22:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -53,5 +53,6 @@ def crack():
 
     return jsonify({'crackedText': cracked_text if cracked_text else 'Not Found'})
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
+
